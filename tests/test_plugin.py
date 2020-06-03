74b4d6e4e0caa3701a5ecbd67694b0fc7b659fc3
pwwang <pwwang@pwwang.com>
2020-06-03 03:27:40 -0500

    Add untracked files

---
+++ tests/test_plugin.py
@@ -0,0 +1,313 @@
+import pytest
+import sys
+import cmdy
+from diot import Diot
+from cmdy import *
+import curio
+
+# testing errors, async and combined use of iter, pipe, redirect
+# normal functions are tested in test_cmdy.py
+
+def test_plugin():
+    @plugin
+    class Plugin:
+        def method(self):
+            pass
+
+    p = Plugin()
+    assert p.enabled
+
+    p.disable()
+    assert not p.enabled
+
+    p.enable()
+    assert p.enabled
+
+def test_plugin_add_method():
+
+    class Base:
+        def __init__(self):
+            self.base = 1
+
+    _hook_class(Base)
+
+    @plugin
+    class Plugin:
+        @add_method(Base)
+        def add(self):
+            return self.base + 1
+
+    p = Plugin()
+    b = Base()
+    assert b.add() == 2
+
+    p.disable()
+    with pytest.raises(AttributeError):
+        b.add()
+
+    # p.enable()
+    # assert b.add() == 2
+
+def test_plugin_add_method_override():
+
+    class Base:
+        def __init__(self):
+            self.base = 1
+
+        def add(self):
+            return self.base + 10
+
+    _hook_class(Base)
+
+    @plugin
+    class Plugin:
+        @add_method(Base)
+        def add(self):
+            return self._original('add')(self) + 1
+
+    p = Plugin()
+    b = Base()
+    assert b.add() == 12
+
+    p.disable()
+    assert b.add() == 11
+
+    # plugins' enabling/disabling should do before calling
+    # p.enable()
+    # assert b.add() == 12
+
+
+def test_plugin_add_method_multi_override():
+
+    class Base:
+        def __init__(self):
+            self.base = 1
+
+        def add(self):
+            return self.base + 10
+
+    _hook_class(Base)
+
+    @plugin
+    class Plugin:
+        @add_method(Base)
+        def add(self):
+            return self._original('add')(self) + 1
+
+    @plugin
+    class Plugin2:
+        @add_method(Base)
+        def add(self):
+            return self._original('add')(self) * 2
+
+    p = Plugin()
+    p2 = Plugin2()
+    b = Base()
+    assert b.add() == 24 # not 22
+
+def test_plugin_add_method_multi_override_disable():
+
+    class Base:
+        def __init__(self):
+            self.base = 1
+
+        def add(self):
+            return self.base + 10
+
+    _hook_class(Base)
+
+    @plugin
+    class Plugin:
+        @add_method(Base)
+        def add(self):
+            return self._original('add')(self) + 1
+
+    @plugin
+    class Plugin2:
+        @add_method(Base)
+        def add(self):
+            return self._original('add')(self) * 2
+
+    b = Base()
+    p = Plugin()
+    p2 = Plugin2()
+    p.disable()
+    assert b.add() == 22
+
+def test_plugin_add_property():
+
+    class Base:
+        def __init__(self):
+            self.base = 1
+    _hook_class(Base)
+
+    @plugin
+    class Plugin:
+        @add_property(Base)
+        def base1(self):
+            return self.base + 1
+
+    p = Plugin()
+    b = Base()
+    assert b.base1 == 2
+
+    p.disable()
+    with pytest.raises(AttributeError):
+        b.base1
+
+    # p.enable()
+    # assert b.base1 == 2
+
+def test_plugin_add_property_override():
+
+    class Base:
+        def __init__(self):
+            self.base = 1
+
+        @property
+        def base1(self):
+            return self.base + 10
+
+    _hook_class(Base)
+
+    @plugin
+    class Plugin:
+        @add_property(Base)
+        def base1(self):
+            return self._original('base1').fget(self) + 1
+
+    p = Plugin()
+    b = Base()
+    assert b.base1 == 12
+
+    p.disable()
+    assert b.base1 == 11
+
+    # p.enable()
+    # assert b.base1 == 12
+
+def test_plugin_add_property_multi_override():
+
+    class Base:
+        def __init__(self):
+            self.base = 1
+
+        @property
+        def base1(self):
+            return self.base + 10
+
+    _hook_class(Base)
+
+    @plugin
+    class Plugin:
+        @add_property(Base)
+        def base1(self):
+            return self._original('base1').fget(self) + 1
+
+    @plugin
+    class Plugin2:
+        @add_property(Base)
+        def base1(self):
+            return self._original('base1').fget(self) * 2
+
+    p = Plugin()
+    p2 = Plugin2()
+    b = Base()
+    assert b.base1 == 24
+
+def test_plugin_add_property_multi_override():
+
+    class Base:
+        def __init__(self):
+            self.base = 1
+
+        @property
+        def base1(self):
+            return self.base + 10
+
+    _hook_class(Base)
+
+    @plugin
+    class Plugin:
+        @add_property(Base)
+        def base1(self):
+            return self._original('base1').fget(self) + 1
+
+    @plugin
+    class Plugin2:
+        @add_property(Base)
+        def base1(self):
+            return self._original('base1').fget(self) * 2
+
+    p = Plugin()
+    p2 = Plugin2()
+    b = Base()
+    p.disable()
+    assert b.base1 == 22
+
+def test_hold_then():
+
+    @plugin
+    class Plugin:
+        @hold_then
+        def test(self):
+            self.stdin = 123
+            return self
+
+    assert callable(Plugin.test.enable)
+    p = Plugin()
+    assert callable(CmdyHolding.test)
+    h = CmdyHolding(*cmdy_util.parse_args('echo', ['echo', '123'], {}), will='test').test()
+    assert h.stdin == 123
+
+def test_run_then():
+
+    @plugin
+    class Plugin:
+        @run_then
+        def ev(self):
+            return eval(self.stdout)
+
+    Plugin()
+    x = cmdy.echo(n = '{"a": 1}').ev()
+    assert x == {"a": 1}
+
+
+def test_final():
+
+    with pytest.raises(cmdy.cmdy_util.CmdyActionError):
+        cmdy.echo().h().fg().iter()
+
+def test_run_then_async():
+
+    @plugin
+    class Plugin:
+
+        @run_then_async('o')
+        async def out(self):
+            ret = []
+            async for line in self:
+                ret.append(line.strip())
+
+            return ''.join(ret)
+
+        @run_then_async
+        async def list(self):
+            ret = []
+            async for line in self:
+                ret.append(line.strip())
+            return ret
+
+    Plugin()
+
+    c = cmdy.echo('1\n2\n3').async_()
+    assert isinstance(c, CmdyAsyncResult)
+
+    assert curio.run(c.rc) == 0
+    assert curio.run(c.rc) == 0 # trigger self._rc is not None
+    assert curio.run(c.out()) == '123'
+
+    c = cmdy.echo('1\n2\n3').async_()
+    assert curio.run(c.list()) == ['1', '2', '3']
+    c = cmdy.echo('12 1>&2', cmdy_shell=True).async_().iter(STDERR)
+    assert curio.run(c.list()) == ['12']
---
+++ tests/test_util.py
@@ -0,0 +1,145 @@
+import pytest
+import time
+import curio
+from diot import Diot
+from cmdy import *
+
+@pytest.mark.parametrize(
+    'args,kwargs,ret_args,ret_kwargs,ret_cfgargs,ret_popenargs', [
+        (("a", "--l=a", {"x": True}), #args
+         {"cmdy_okcode": 1, "cmdy_encoding": "utf-8", "y": False}, #kwargs
+         ["a", "--l=a"], # ret_args
+         {"x": True}, # ret_kwargs
+         {"okcode": [1], "encoding": "utf-8"}, # ret_cfgargs
+         {}), # ret_popenargs
+        (("a", "--l=a", {"x": True}), #args
+         {"cmdy_okcode": 1, "cmdy_encoding": "utf-8",
+          "y": False, "popen_close_fds": True}, #kwargs
+         ["a", "--l=a"], # ret_args
+         {"x": True}, # ret_kwargs
+         {"okcode": [1], "encoding": "utf-8"}, # ret_cfgargs
+         {"close_fds": True}), # ret_popenargs
+        (("a", "--l=a", {"x": True}), #args
+         {"cmdy_okcode": 1, "cmdy_encoding": "utf-8", "cmdy_shell": "/bin/sh",
+          "y": False, "popen_close_fds": True}, #kwargs
+         ["a", "--l=a"], # ret_args
+         {"x": True}, # ret_kwargs
+         {"okcode": [1], "encoding": "utf-8", "shell": ["/bin/sh", "-c"]}, # ret_cfgargs
+         {"close_fds": True}), # ret_popenargs
+        (("a", "--l=a", {"x": True}), #args
+         {"cmdy_okcode": 1, "cmdy_encoding": "utf-8", "cmdy_shell": ["/bin/sh"],
+          "y": False, "popen_close_fds": True}, #kwargs
+         ["a", "--l=a"], # ret_args
+         {"x": True}, # ret_kwargs
+         {"okcode": [1], "encoding": "utf-8", "shell": ["/bin/sh", "-c"]}, # ret_cfgargs
+         {"close_fds": True}), # ret_popenargs
+    ]
+)
+def test_parse_args(args, kwargs, ret_args, ret_kwargs,
+                    ret_cfgargs, ret_popenargs):
+
+    x_args, x_kwargs, x_cfgargs, x_popenargs = cmdy_util.parse_args('', args, kwargs)
+    assert x_args == ret_args
+    assert x_kwargs == ret_kwargs
+    for key, val in ret_cfgargs.items():
+        assert x_cfgargs[key] == val
+    assert x_popenargs == ret_popenargs
+    assert isinstance(x_popenargs, Diot)
+    assert isinstance(x_cfgargs, Diot)
+
+def test_parse_args_warnings():
+
+    with pytest.warns(UserWarning):
+        args, kwargs, _, _ = cmdy_util.parse_args('', args=[{"x":1}],
+                                                  kwargs={'x': 2})
+    assert args == []
+    assert kwargs == {'x': 2}
+
+    with pytest.warns(UserWarning):
+        cmdy_util.parse_args('', args=[], kwargs={'popen_stdin': 2})
+    with pytest.warns(UserWarning):
+        cmdy_util.parse_args('', args=[], kwargs={'popen_encoding': None})
+    with pytest.warns(UserWarning):
+        cmdy_util.parse_args('', args=[], kwargs={'popen_shell': True})
+
+
+def test_will():
+
+    def wrapper():
+        will = cmdy_util.will()
+        return Diot(x=1, y=lambda: True, will=will)
+
+    assert wrapper().x == 1
+    will = wrapper().will
+    assert will == 'will'
+
+    will = wrapper()['will']
+    assert will is None
+
+def test_asnyc_to_sync():
+
+    p = curio.subprocess.Popen(['echo', '-e', '1\\n2\\n3'],
+                               stdout=curio.subprocess.PIPE)
+    stream = cmdy_util.SyncStreamFromAsync(p.stdout)
+    assert stream.dump() == b'1\n2\n3\n'
+
+    p = curio.subprocess.Popen(['echo', '-e', '1\\n2\\n3'],
+                               stdout=curio.subprocess.PIPE)
+    stream = cmdy_util.SyncStreamFromAsync(p.stdout, encoding='utf-8')
+    assert stream.dump() == '1\n2\n3\n'
+
+    p = curio.subprocess.Popen(['echo', '-e', '1\\n2\\n3'],
+                               stdout=curio.subprocess.PIPE)
+
+    stream = cmdy_util.SyncStreamFromAsync(p.stdout)
+    assert next(stream) == b'1\n'
+    assert next(stream) == b'2\n'
+    assert next(stream) == b'3\n'
+
+    with pytest.raises(StopIteration):
+        next(stream)
+
+    p = curio.subprocess.Popen(['echo', '-e', '1\\n2\\n3'],
+                               stdout=curio.subprocess.PIPE)
+
+    stream = cmdy_util.SyncStreamFromAsync(p.stdout, encoding='utf-8')
+    assert next(stream) == '1\n'
+    assert next(stream) == '2\n'
+    assert next(stream) == '3\n'
+
+    with pytest.raises(StopIteration):
+        next(stream)
+
+    # test blocking
+    p = curio.subprocess.Popen(['bash', '-c',
+                                'echo -e "1\\n2\\n3"; sleep .2; echo 4'],
+                               stdout=curio.subprocess.PIPE)
+
+    tic = time.time()
+    stream = iter(cmdy_util.SyncStreamFromAsync(p.stdout, encoding='utf-8'))
+
+    assert next(stream) == '1\n'
+    assert next(stream) == '2\n'
+    assert next(stream) == '3\n'
+    assert time.time() - tic < .3
+    assert next(stream) == '4\n'
+    assert time.time() - tic > .2
+
+@pytest.mark.parametrize('args,kwargs,shell,prefix,sep,dupkey,expected', [
+    (['ls'], {}, ['bash', '-c'], 'auto', 'auto', False, ['bash', '-c', 'ls']),
+    (['ls'], {}, False, 'auto', 'auto', False, ['ls']),
+    (['bedtools', 'intersect', '-a', 'file'], {'bfile': 'file2'}, False,
+     'auto', 'auto', False,
+     ['bedtools', 'intersect', '-a', 'file', '--bfile=file2']),
+    (['abc', 'def'], {'': 'file2', 'k': [1,2], 'xyz': [3,4], '_': 'end'},
+     False, 'auto', 'auto', False,
+     ['abc', 'def', 'file2', '-k', '1', '2', '--xyz=3', '4', 'end']),
+    (['abc', 'def'], {'': 'file2', 'k': [1,2], 'xyz': [3,4], '_': 'end'},
+     False, 'auto', 'auto', True,
+     ['abc', 'def', 'file2', '-k', '1', '-k', '2',
+      '--xyz=3', '--xyz=4', 'end']),
+])
+def test_compose_cmd(args, kwargs, shell, prefix, sep, dupkey, expected):
+    assert cmdy_util.compose_cmd(args, kwargs,
+                            shell=shell, prefix=prefix, sep=sep,
+                            dupkey=dupkey) == expected
